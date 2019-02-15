e1246e956b1a41514f9fa4bdf593b0c436d171fe
Joseph Hunkeler <jhunkeler@gmail.com>
2019-02-15 08:25:51 -0500

    Timeout messages cannot be seen anyway.
    * Use SIGTERM instead of SIGKILL -- again

--- spawner.sh
+++ spawner.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 limit="${TIMEOUT:-45m}"
-echo "This job will automatically terminate after: ${limit}"
-timeout -sKILL ${limit} $@
-retval=$?
-if [[ ${retval} == 124 ]] || [[ ${retval} == 137 ]]; then
-    echo "Job terminated: ${limit} time limit reached"
-fi
-exit ${retval}
+exec timeout -sTERM ${limit} "$@"
